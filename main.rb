2ba0903e988b96d9a89a970132e42510d63600d1
leo-holanda <leo.uldholanda@gmail.com>
2019-08-13 11:05:57 -0300

    add caesar cipher algorithm

--- main.rb
+++ main.rb
@@ -1,5 +1,42 @@
 require 'sinatra'
 
+def caesar_cipher(input, shift_factor)
+ 
+  output = []
+  input = input.split('')
+  shift_factor = shift_factor.to_i
+
+  input.each { |char|
+  
+      converted_char = char.ord
+      caesar_char = converted_char + shift_factor
+  
+      if (converted_char >= 97) && (converted_char <= 122)
+          if caesar_char > 122
+              caesar_char = (caesar_char - 122) + 96
+          end
+      
+      elsif (converted_char >= 65) && (converted_char <= 90)
+          if caesar_char > 90
+              caesar_char = (caesar_char - 90) + 64
+          end
+      elsif (converted_char >= 48) && (converted_char <= 57)
+          if caesar_char > 57
+              caesar_char = (caesar_char - 57) + 47
+          end
+      else
+          caesar_char = char
+      end
+
+      output.push(caesar_char.chr)
+  }
+
+  return output.join("")
+end
+
 get '/' do
-  erb :index
+  user_input = params['user_input']
+  shift_factor = params['shift_factor']
+  encoded_input = caesar_cipher(user_input, shift_factor)
+  erb :index, :locals => {:encoded_input => encoded_input}
 end
